f106d7e49e8f1e869f49c32e6214833a5f79de29
N103Gremillion <--global>
2024-10-20 21:22:53 -0500

    I got the training of the csv working

--- Main.java
+++ Main.java
@@ -5,43 +5,50 @@ import java.util.Random;
 
 class Main {
 
-    static double learningRate = 3.000;
+    static double learningRate = 1;
     static int totalLayers = 3;
-    static int totalEpochs = 30;
+    static int totalEpochs = 60;
     static int miniBatchSize = 10;
     static int inputLinesSize = 785;
-    static double[][] layer1weights;
-    static double[][] layer2weights;
-    static double[][] layer1biases;
-    static double[][] layer2biases;
+    static int totalDataPoints = 60000;
+    static int totalMiniBatches = totalDataPoints / miniBatchSize;
+    static Matrix layer1weights;
+    static Matrix layer2weights;
+    static Matrix layer1biases;
+    static Matrix layer2biases;
     // this will hold all the csv data in a readable format for my setup
-    static NeuralNetwork[][] trainingNetworks = new NeuralNetwork[totalEpochs][miniBatchSize];
+    static NeuralNetwork[][] trainingNetworks;
     // this corresponds to the trainingNetworks and has the expected values using the label of the 1st value in each csv row
-    static Matrix[][] expectedOutputsOfTrainingNetworks = new Matrix[totalEpochs][miniBatchSize];
+    static Matrix[][] expectedOutputsOfTrainingNetworks;
     
     public static void main(String args[]) {
+        
+        // initalize the training network / expected outputs
+
+        trainingNetworks = new  NeuralNetwork[totalMiniBatches][miniBatchSize];
+        expectedOutputsOfTrainingNetworks = new Matrix[totalMiniBatches][miniBatchSize];
 
         readCSV("mnist_train.csv");
         setupInitialWeightsAndBiases();
-        
+
         // train the network from the mnist_train.csv (uses 10 as the minibatach aka. 1 row of the trainingNetworks)
-        System.out.println("layer 2 weights before 1st epoch");
+        for (int curEpoch = 1; curEpoch <= totalEpochs; curEpoch++){
+            runEpoch(curEpoch, trainingNetworks, expectedOutputsOfTrainingNetworks);
+        }
 
-        print2dArray(layer2biases);
-        
-        System.out.println("layer 2 weights after 1st epoch");
-
-        print2dArray(layer1biases);
-
-        runEpoch(1, trainingNetworks[0], expectedOutputsOfTrainingNetworks[0]);
     }
 
     public static void setupInitialWeightsAndBiases(){
         // fill the wieghts with random values from -1 -> 1 (note : this is 15x784 since layer0 is 784 and layer 1 is 15)
-        layer1weights = initalizeWeightsOrBiases(15, 784);
-        layer2weights = initalizeWeightsOrBiases(10, 15);
-        layer1biases = initalizeWeightsOrBiases(15, 1);
-        layer2biases = initalizeWeightsOrBiases(10, 1);
+        double[][] layer1weightsValues = initalizeWeightsOrBiases(15, 784);
+        double[][] layer2weightsValues = initalizeWeightsOrBiases(10, 15);
+        double[][] layer1biasesValues = initalizeWeightsOrBiases(15, 1);
+        double[][] layer2biasesValues = initalizeWeightsOrBiases(10, 1);
+
+        layer1weights = new Matrix(layer1weightsValues);
+        layer2weights = new Matrix(layer2weightsValues);
+        layer1biases = new Matrix(layer1biasesValues);
+        layer2biases = new Matrix(layer2biasesValues);
     }
 
     // fill a 2d array with vlalues form -1 to 1
@@ -73,7 +80,7 @@ class Main {
             while ((line = reader.readLine()) != null) {
 
                 // if your hit the 30 epoch total
-                if (curLine >= totalEpochs * miniBatchSize) {
+                if (curLine >= 60000) {
                     break;
                 }
 
@@ -110,9 +117,13 @@ class Main {
         double[][] trainingNetworkInputs = new double[inputLinesSize - 1][1];
         for (int i = 1; i < inputLinesSize; i++){
             int curIntValue = Integer.parseInt(networkInputs[i]);
-            trainingNetworkInputs[i - 1][0] = (double) curIntValue;
+            double scaledValue = curIntValue / 255.0;
+            trainingNetworkInputs[i - 1][0] = scaledValue;
         }
+
         NeuralNetwork trainingNetwork = new NeuralNetwork(trainingNetworkInputs, 0, totalLayers);
+        trainingNetworks[row][column] = trainingNetwork;
+
     }
 
     // returns the oneHotValue of the integer that represents the expected output
@@ -138,17 +149,14 @@ class Main {
         return new Matrix(values);
     }
 
-    public static void print2dArray(double[][] array2d){
-
-        for (int i = 0; i < array2d.length; i++) { 
-
-            for (int j = 0; j < array2d[i].length; j++) {  
-                System.out.print(array2d[i][j] + " ");  
+    public static void print2dArray(double[][] array2d) {
+        // Print the 2D array
+        for (double[] row : array2d) {
+            for (double value : row) {
+                System.out.print(value + " ");
             }
-
-        System.out.println();  
+            System.out.println(); 
         }
-
     }
 
     // note it is assumed that the networks are the same sizes but different cases
@@ -171,60 +179,112 @@ class Main {
         }
     }
     
-    // function to setup and test the inputs/weights/biases in the excel file (kinda the entry point for main to reference)
-    public static void runEpoch(int epochNum, NeuralNetwork[] networks, Matrix[] expecteds){
+    // use the output activation and find the one with largest probablity
+    private static int getPredictedOutput(Matrix outputLayer) {
         
-        // values to keep track of accuracy of the epoch
+        int curPrediction = 0;
+        double highestProbability = outputLayer.grid[0][0];
+
+        for (int i = 0; i < outputLayer.grid.length; i++){
+            double curProbability = outputLayer.grid[i][0];
+            if (curProbability > highestProbability){
+                highestProbability = curProbability;
+                curPrediction = i;
+            }
+        }
+        
+        return curPrediction;
+
+    }
+
+    // convert the 1 hot value to int representation
+    private static int getIntOfHotValue(Matrix currentExpectedOutput){
+        int result = 0;
+
+        for (int i = 0; i < currentExpectedOutput.grid.length; i++){
+            if (currentExpectedOutput.grid[i][0] ==  1){
+                result = i;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    // function to setup and test the inputs/weights/biases in the excel file (kinda the entry point for main to reference)
+    public static void runEpoch(int epochNum, NeuralNetwork[][] networks, Matrix[][] expecteds) {
         int totalCorrectPredictions = 0;
         int totalPredictions = 0;
         int[] correctsForEachNum = new int[10];
-        int[] totalForEachNum = new int[];
+        int[] totalForEachNum = new int[10];
 
-        //********************************Epoc 1***************************************** */
-        for (int caseNum = 0; caseNum < networks.length; caseNum++) {
-            NeuralNetwork currentNetwork = networks[caseNum];
-            Matrix currentExpectedOutput = expecteds[caseNum];
+        System.out.println(String.format("************************************ EPOCH %d **********************************************", epochNum));
 
-            // Forward pass for both layers
-            currentNetwork.forwardPass(currentNetwork.layers[0].activationValues, new Matrix(layer1weights), new Matrix(layer1biases), 1);
-            currentNetwork.forwardPass(currentNetwork.layers[1].activationValues, new Matrix(layer2weights), new Matrix(layer2biases), 2);
+        for (int curRow = 0; curRow < networks.length; curRow++) {
+            for (int caseNum = 0; caseNum < miniBatchSize; caseNum++) {
+                NeuralNetwork currentNetwork = networks[curRow][caseNum];
+                Matrix currentExpectedOutput = expecteds[curRow][caseNum];
 
-            // update the cout of the num / correctly predicted num
-            int predictedNum = 
-            if (pr)
-            // Backpropagation
-            for (int curLayer = totalLayers; curLayer > 1; curLayer--) {
-                currentNetwork.backwardPropogate(currentExpectedOutput, curLayer);
+                // Forward pass
+                currentNetwork.forwardPass(currentNetwork.layers[0].activationValues, layer1weights, layer1biases, 1);
+                currentNetwork.forwardPass(currentNetwork.layers[1].activationValues, layer2weights, layer2biases, 2);
+
+                // Get predictions 
+                int predictedOutput = getPredictedOutput(currentNetwork.layers[2].activationValues);
+                int expectedIntegerOutput = getIntOfHotValue(currentExpectedOutput);
+
+                // Backpropagation
+                for (int curLayer = totalLayers; curLayer > 1; curLayer--) {
+                    currentNetwork.backwardPropogate(currentExpectedOutput, curLayer);
+                }
+
+                if (predictedOutput == expectedIntegerOutput) {
+                    totalCorrectPredictions++;
+                    correctsForEachNum[predictedOutput]++;
+                }
+                totalPredictions++;
+                totalForEachNum[expectedIntegerOutput]++;
             }
-        }
-        /***********************adjust the weights after all 10 cases for this minibatch********************************/ 
-        Matrix[] layer2WeightGradients = new Matrix[networks.length];
-        Matrix[] layer1WeightGradients = new Matrix[networks.length];
-        Matrix[] layer2BiasGradients = new Matrix[networks.length];
-        Matrix[] layer1BiasGradients = new Matrix[networks.length];
 
-        // Loop through each network and populate the arrays
-        for (int i = 0; i < networks.length; i++) {
-            layer2WeightGradients[i] = networks[i].layers[2].weightGradient;
-            layer1WeightGradients[i] = networks[i].layers[1].weightGradient;
-            layer2BiasGradients[i] = networks[i].layers[2].biasGradient;
-            layer1BiasGradients[i] = networks[i].layers[1].biasGradient;
+            // Adjust the weights after processing the mini-batch
+            updateWeightsForMiniBatch(networks[curRow]);
         }
 
-        updateUsingGradients(learningRate, 10, layer2weights, layer2WeightGradients);
-        updateUsingGradients(learningRate, 10, layer1weights, layer1WeightGradients);
-        updateUsingGradients(learningRate, 10, layer2biases, layer2BiasGradients);
-        updateUsingGradients(learningRate, 10, layer1biases, layer1BiasGradients);
-        
-        System.out.println("\nThe Update weights / biases after this minibatch are : \n");
+        // Display epoch accuracy
+        double accuracy = (double) totalCorrectPredictions / totalPredictions * 100;
+        System.out.printf("Total accuracy of Epoch %d: %d / %d (%.2f%%)%n", epochNum, totalCorrectPredictions, totalPredictions, accuracy);
+        // accuracies for each of the numbers
+        for (int i = 0; i < correctsForEachNum.length; i++){
+            double curNumAccuracy = (double) correctsForEachNum[i] / totalForEachNum[i] * 100;
+            System.out.println(String.format("The accuracy of %d : %d / %d or %.2f", i, correctsForEachNum[i], totalForEachNum[i], curNumAccuracy));
+        }
 
-        System.out.println("\n************** Layer 1 Weights ******************\n");
-        print2dArray(layer1weights);
-        System.out.println("\n************** Layer 2 Weights ******************\n");
-        print2dArray(layer2weights);
-        System.out.println("\n************** Layer 1 Biases ******************\n");
-        print2dArray(layer1biases);
-        System.out.println("\n************** Layer 2 Biases ******************\n");
-        print2dArray(layer2biases);
+        System.out.println(String.format("************************************ End of Epoch %d **********************************************", epochNum));
     }
+
+
+    /**
+     * Update weights and biases using gradients collected from a mini-batch of networks.
+     */
+    private static void updateWeightsForMiniBatch(NeuralNetwork[] miniBatch) {
+        Matrix[] layer2WeightGradients = new Matrix[miniBatch.length];
+        Matrix[] layer1WeightGradients = new Matrix[miniBatch.length];
+        Matrix[] layer2BiasGradients = new Matrix[miniBatch.length];
+        Matrix[] layer1BiasGradients = new Matrix[miniBatch.length];
+
+        // Collect gradients from each network in the mini-batch
+        for (int i = 0; i < miniBatch.length; i++) {
+            layer2WeightGradients[i] = miniBatch[i].layers[2].weightGradient;
+            layer1WeightGradients[i] = miniBatch[i].layers[1].weightGradient;
+            layer2BiasGradients[i] = miniBatch[i].layers[2].biasGradient;
+            layer1BiasGradients[i] = miniBatch[i].layers[1].biasGradient;
+        }
+
+        // Update weights and biases
+        updateUsingGradients(learningRate, miniBatch.length, layer2weights.grid, layer2WeightGradients);
+        updateUsingGradients(learningRate, miniBatch.length, layer1weights.grid, layer1WeightGradients);
+        updateUsingGradients(learningRate, miniBatch.length, layer2biases.grid, layer2BiasGradients);
+        updateUsingGradients(learningRate, miniBatch.length, layer1biases.grid, layer1BiasGradients);
+    }
+
 }
